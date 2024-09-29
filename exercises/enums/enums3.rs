e0c4186a015517cbb292840f464c7d284a05976b
root <root@Sanyic.localdomain>
2024-09-30 00:54:02 +0800

    update enums

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -4,11 +4,11 @@
 //
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Move (Point),
+    Echo(String),
+    ChangeColor (u8, u8, u8),
+    Quit
 }
 
 struct Point {
@@ -43,6 +43,14 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(a, b, c) => {
+                self.color = (a, b, c)
+            },
+            Message::Echo(str) => self.message = str,
+            Message::Quit => self.quit = true,
+            Message::Move(pot) => self.position = pot, 
+        }
     }
 }
 
