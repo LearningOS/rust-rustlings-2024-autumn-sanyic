ae59345d1618f1e05d13a19c6cedc51efde5a8e8
root <root@Sanyic.localdomain>
2024-10-01 19:06:14 +0800

    update hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,10 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Clone, Copy)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,6 +38,30 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let mut team_local = Team{
+            goals_conceded: 0,
+            goals_scored: 0
+        };
+
+        if scores.get(&team_1_name).is_some() {
+            team_local = *scores.get(&team_1_name).unwrap();
+        }
+
+        let team_1 = Team{
+            goals_conceded: team_2_score + team_local.goals_conceded,
+            goals_scored: team_1_score + team_local.goals_scored
+        };
+
+        if scores.get(&team_2_name).is_some() {
+            team_local = *scores.get(&team_2_name).unwrap();
+        }
+
+        let team_2 = Team{
+            goals_conceded: team_1_score + team_local.goals_conceded,
+            goals_scored: team_2_score + team_local.goals_scored
+        };
+        scores.insert(team_1_name, team_1);
+        scores.insert(team_2_name, team_2);
     }
     scores
 }
